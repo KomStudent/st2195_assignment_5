5edb75829f09aaec09c57ec62c38f996c9fe7cb4
Karl Munroe <kom9@student.london.ac.uk>
2023-01-30 07:12:34 -0500

    Exercise Completed
    Code completed in foo.py.
    
    Readme.MD also updated

--- foo.py
+++ foo.py
@@ -1,18 +1,10 @@
-# st2195_assignment_5
- ST2196 Assignment No. 5
- 
-This assignment corrects the code in the Practice Assignment 5. The purpose of the code 
-is to:
-* Define a function is_divisible_by_k(x,y) that takes two arguments and determines if x is divisible by y
-* Create a list of all numbers from 1 to 1000 inclusive that are divisibly by 2, 5 and 7
-* Make a sum of the numbers
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 30 06:39:31 2023
 
-An additional step was done to print the list.
+@author: Karl.Munroe
+"""
 
-The code will be published in a file called foo.py that is part of this repository
-
-Code below
-```{Python}
 def is_divisible_by_k(x, k):
  '''
  Checks whether x is divisible by k.
@@ -30,7 +22,7 @@ Store all the integers that are multiples of 2 or 5 or 7 that are lower
 or equal to 1000 (excluding doubles)
 '''
 x = [] # create a blank list
-for i in range(1,1001,1): #KM - Use the range from 1 to 1000, 1001 is not included    
+for i in range(1,1001,1): #KM - Use the range from 1 to 1000, 1001 is not included       
  if (is_divisible_by_k(i, 2) | is_divisible_by_k(i, 5)) |is_divisible_by_k(i, 7):
      x.append(i)
 
@@ -44,4 +36,3 @@ print(x)
 totalSum = sum(x)
 print("The total of all elements is: %2d" % totalSum)
 
-```
